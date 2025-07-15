c5e5c21479b39a0e81e84b514de543d8bb082c7e
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-07-15 09:29:51 +0545

    langgraph studio

--- 4-Debugging/agent.py
+++ 4-Debugging/agent.py
@@ -22,3 +22,42 @@ os.environ["LANGSMITH_TRACING"] = "true"
 os.environ["LANGSMITH_PROJECT"] = "TestProject"
 
 llm = init_chat_model("groq:llama3-8b-8192")
+
+class State(TypedDict):
+    messages:Annotated[list[BaseMessage], add_messages]
+
+def make_tool_graph():
+    ## Graph with tool call
+
+    @tool
+    def add(a:float, b:float):
+        """Add two numbers"""
+        return a+b
+
+    tools = [add]
+    tool_node = ToolNode([add])
+
+    llm_with_tool = llm.bind_tools([add])
+
+    def call_llm_model(state:State):
+        return {"messages": [llm_with_tool.invoke(state["messages"])]}
+    
+    ## Graph
+    builder = StateGraph(State)
+    builder.add_node("tool_calling_llm", call_llm_model)
+    builder.add_node("tools", ToolNode(tools))
+
+    ## Add Edges
+    builder.add_edge(START, "tool_calling_llm")
+    builder.add_conditional_edges(
+        "tool_calling_llm",
+        tools_condition
+    )
+    builder.add_edge("tools", "tool_calling_llm")
+
+    ## compike the graph
+    graph = builder.compile()
+    return graph
+
+tool_agent = make_tool_graph()
+
